0fe979f7b97824e910094ecd5007eddc3d27fd9e
vannxavero <vannxaveroo@proton.me>
2025-09-29 03:24:39 +0700

    CI: Bump to 29092025 build
    Clang version: DiscussionVersePrjkt clang version 22.0.0git (https://github.com/llvm/llvm-project 41f3438362f5ae2a06544fec7db18c37f5ecd79b)
    LLVM repo commit: [VPlan] Remove dead code for scalar VFs in VPRegionBlock::cost (NFC).
    Link: https://github.com/llvm/llvm-project/commit/41f3438362f5ae2a06544fec7db18c37f5ecd79b
    
    Change-Id: Id95da75cba4b53e6e657f38bb58572feb1498ec0
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

---
+++ get_latest_url.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+LATEST_URL_GZ=https://github.com/xaverodumpster/dv_clang/releases/download/29092025/DiscussionVerse-Clang-22.0.0git-29092025.tar.gz
+LATEST_URL_XZ=https://github.com/xaverodumpster/dv_clang/releases/download/29092025/DiscussionVerse-Clang-22.0.0git-29092025.tar.xz
